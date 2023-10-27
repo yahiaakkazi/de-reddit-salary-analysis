cee6423f541fab32aefd635aedaeac1d5b926a83
Yahia AKKAZI <yahyaakkazi@gmail.com>
2023-10-27 16:41:08 +0200

    Adding 4 columns to display relevant stats of the dataframe

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ with col1:
                 Welcome to the ***Data Engineering Salary Analysis*** web app! This platform provides
                 a **comprehensive exploration** of **salary** information and relevant data for **data 
                 engineering roles worldwide**. The dataset is curated based on **searches and submissions**
-                from the Reddit community, specifically the ***"Salary on the Data Engineering"*** subreddit.""")
+                from the Reddit community, specifically the ***"Salary on the Data Engineering"*** subreddit.
+                Whenever this app is run through the docker file, the reddit scraper and openai"s gpt crawl data
+                from subredit data engineering salary submissions and then this webapp gets deployed. In other words,
+                these results are completely """)
     # st.markdown('''
     #     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
     #     :gray[pretty] :rainbow[colors].''')
@@ -27,14 +30,28 @@ with col1:
 df_salary_per_country = df[["Country","Base Salary"]]
 df_salary_per_country["Base Salary"] = pd.to_numeric(df_salary_per_country["Base Salary"], errors="coerce")
 df_salary_per_country = df_salary_per_country.groupby(by="Country",as_index=False).agg({"Base Salary":"mean"})
-
+df_num = df[["Country","Base Salary"]]
+df_num["Base Salary"] = pd.to_numeric(df_salary_per_country["Base Salary"], errors="coerce")
+mean = df_num[["Base Salary"]].mean().values[0]
+medi = df_num[["Base Salary"]].median().values[0]
+rounded_mean = round(mean/1000,1)
+rounded_medi = round(medi/1000,1)
+rounded_min = round(df_num[["Base Salary"]].min().values[0]/1000,1)
+rounded_max = round(df_num[["Base Salary"]].max().values[0]/1000,1)
 with col2:
     st.bar_chart(data=df_salary_per_country, x="Country", y="Base Salary")
+    coll1, coll2, coll3 ,coll4 = st.columns(4)
+    coll1.metric("Avg. Salary", f'{rounded_mean}K $', "1.2 °F")
+    coll2.metric("Median Sal", f'{rounded_medi}K $', "-8%")
+    coll3.metric("Min Salary", f'{rounded_min}K $', "4%")
+    coll4.metric("Max Salary", f'{rounded_max}K $', "4%")
 
 st.sidebar.header("Contact Information")
 st.sidebar.markdown("- Email: [yahia.akkazi@centrale-marseille.fr](mailto:yahia.akkazi@centrale-marseille.fr)")
 st.sidebar.markdown("- Phone: +33 7 50 58 50 55")
 st.sidebar.markdown("- Portfolio: [My Portfolio](http://akkazi-portfolio.ddnsgeek.com:3000/)")
+
+
 
 st.markdown(
         """
