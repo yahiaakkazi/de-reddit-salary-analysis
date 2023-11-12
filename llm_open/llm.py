2b40f928c594b717945de1d54dfef822e0dfd0c0
Yahia AKKAZI <yahyaakkazi@gmail.com>
2023-11-12 01:52:10 +0100

    Typing modificatioons on llm.py

--- llm_open/llm.py
+++ llm_open/llm.py
@@ -1,21 +1,26 @@
+from typing import Optional
 import openai
 
 
-class LLM():
-    def __init__(self, llm_model, system_request, user_request):
+class LLM:
+    """class to initialise LLM and send requests to it"""
+
+    def __init__(self, llm_model, system_request, user_request) -> None:
         while True:
             try:
                 self.completion = openai.ChatCompletion.create(
                     model=llm_model,
                     messages=[
                         {"role": "system", "content": system_request},
-                        {"role": "user", "content": user_request}
+                        {"role": "user", "content": user_request},
                     ],
-                    temperature = 0
+                    temperature=0,
                 )
                 break
             except:
                 print("Timedout, retrying...", end="\r")
                 continue
-    def get_message(self):
+
+    def get_message(self) -> Optional[str]:
+        """returns the message content"""
         return self.completion.choices[0].message.content
